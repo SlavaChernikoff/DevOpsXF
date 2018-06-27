2c461daa6a53db03a6aa1f7957ee6d125ad235ff
Slava Chernikoff <slava.chernikoff@binwell.com>
2018-06-27 19:30:19 +0300

    Consolidate XF libs

--- DevOpsXF/App.cs
+++ DevOpsXF/App.cs
@@ -1,12 +1,6 @@
-using System;
 using DevOpsXF.DAL;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-using Microsoft.AppCenter.Distribute;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace DevOpsXF
@@ -14,15 +8,7 @@ namespace DevOpsXF
 	public class App : Application {
 		public App() {
 			MainPage = new NavigationPage(new MainPage());
-			DataService.Instance.Init("http://data.fixer.io", "8754892b614a90b5cb1debdf6948e7dc");
-		}
-
-		protected override void OnStart() {
-			AppCenter.Start("ios={Your Xamarin iOS App Secret};" +
-			                "android=cbd6369e-9944-4284-9bff-3cf550ab7907;",
-				typeof(Analytics), typeof(Crashes), typeof(Distribute));
-
-			Crashes.NotifyUserConfirmation(UserConfirmation.AlwaysSend);
+			DataService.Instance.Init("http://apilayer.net", "1809397aeb646717dc0f4b44f820d81c");
 		}
 	}
 }
